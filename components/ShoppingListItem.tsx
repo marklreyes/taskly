38a60e587a0d9ced13a435a08a701dff7159d9df
Mark Reyes <mr@marklreyes.com>
2024-12-27 16:43:25 -0800

    Create isCompleted as an optional prop passed into ShoppingListItem
    This will help visually discern what is completed on the shopping list.

--- components/ShoppingListItem.tsx
+++ components/ShoppingListItem.tsx
@@ -3,8 +3,10 @@ import { theme } from "../theme";
 
 type Props = {
   name: string;
+  isCompleted?: boolean;
 };
-export function ShoppingListItem({ name }: Props) {
+
+export function ShoppingListItem({ name, isCompleted }: Props) {
   const handleDelete = () => {
     Alert.alert(
       `Are you sure you want to delete ${name}?`,
@@ -23,10 +25,25 @@ export function ShoppingListItem({ name }: Props) {
     );
   };
   return (
-    <View style={styles.itemContainer}>
-      <Text style={styles.itemText}>{name}</Text>
+    <View
+      style={[
+        styles.itemContainer,
+        isCompleted ? styles.completedContainer : undefined,
+      ]}
+    >
+      <Text
+        style={[
+          styles.itemText,
+          isCompleted ? styles.completedText : undefined,
+        ]}
+      >
+        {name}
+      </Text>
       <TouchableOpacity
-        style={styles.button}
+        style={[
+          styles.button,
+          isCompleted ? styles.completedButton : undefined,
+        ]}
         onPress={handleDelete}
         activeOpacity={0.8}
       >
@@ -45,6 +62,18 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
+  },
+  completedContainer: {
+    backgroundColor: theme.colorLightGrey,
+    borderBottomColor: theme.colorLightGrey,
+  },
+  completedButton: {
+    backgroundColor: theme.colorGrey,
+  },
+  completedText: {
+    textDecorationLine: "line-through",
+    textDecorationColor: theme.colorGrey,
+    color: theme.colorGrey,
   },
   itemText: {
     fontSize: 18,
