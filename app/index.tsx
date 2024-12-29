cada8fcbdcedfad72edba345c10802aae4b35976
Mark Reyes <mr@marklreyes.com>
2024-12-28 21:46:29 -0800

    Add TextInput and useState to replace hardcoded ShoppingListItem in View

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,47 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "Coffee" },
+  { id: "2", name: "Tea" },
+  { id: "3", name: "Sugar" },
+];
 
 export default function App() {
+  const [shoppingList, setShoppingList] =
+    useState<ShoppingListItemType[]>(initialList);
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {
+    if (!value) return;
+    const newItem: ShoppingListItemType = {
+      id: Date.now().toString(),
+      name: value,
+    };
+    setShoppingList([newItem, ...shoppingList]);
+    setValue("");
+  };
   return (
     <View style={styles.container}>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" isCompleted />
-      <ShoppingListItem name="Sugar" isCompleted />
+      <TextInput
+        style={styles.textInput}
+        placeholder="E.g. Coffee"
+        value={value}
+        onChangeText={setValue}
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+      />
+      {shoppingList.map((item) => (
+        <ShoppingListItem key={item.id} name={item.name} />
+      ))}
       <StatusBar style="auto" />
     </View>
   );
@@ -18,6 +51,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+    paddingTop: 12,
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: theme.colorLightGrey,
+    borderRadius: 50,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
   },
 });
