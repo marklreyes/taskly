bc3a32fb127b5bdb2d994b4b5cc5d6bcb7f8aaf4
Mark Reyes <mr@marklreyes.com>
2024-12-31 14:31:29 -0800

    Create a 5 second scheduled notification from Counter screen

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,20 +1,41 @@
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from "react-native";
 import { theme } from "../../theme";
 import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 
 export default function CounterScreen() {
-  const handleRequestPermission = async () => {
+  const scheduleNotification = async () => {
     const result = await registerForPushNotificationsAsync();
-    console.log(result);
+    if (result === "granted") {
+      console.log(result);
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "I'm a notification from your app.",
+        },
+        trigger: {
+          seconds: 5,
+          type: "timeInterval",
+          repeats: false,
+        },
+      });
+    } else {
+      if (Device.isDevice) {
+        Alert.alert(
+          "Unable to schedule notification.",
+          "Enable the notification permission for Expo Go in the settings.",
+        );
+      }
+    }
   };
   return (
     <View style={styles.container}>
       <TouchableOpacity
         style={styles.button}
         activeOpacity={0.8}
-        onPress={handleRequestPermission}
+        onPress={scheduleNotification}
       >
-        <Text style={styles.buttonText}>Request permission</Text>
+        <Text style={styles.buttonText}>Schedule a notification</Text>
       </TouchableOpacity>
     </View>
   );
