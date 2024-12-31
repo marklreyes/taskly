6fa5c3c8db59a0453252aea3a4a3f14e82fea95f
Mark Reyes <mr@marklreyes.com>
2024-12-31 14:50:11 -0800

    Testing out a rendering a UI that updates every second

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -3,8 +3,19 @@ import { theme } from "../../theme";
 import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
+import { useEffect, useState } from "react";
 
 export default function CounterScreen() {
+  const [secondsElapsed, setSecondsElapsed] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSecondsElapsed((val) => val + 1);
+    }, 1000);
+    // Clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
+
   const scheduleNotification = async () => {
     const result = await registerForPushNotificationsAsync();
     if (result === "granted") {
@@ -30,6 +41,7 @@ export default function CounterScreen() {
   };
   return (
     <View style={styles.container}>
+      <Text>{secondsElapsed}</Text>
       <TouchableOpacity
         style={styles.button}
         activeOpacity={0.8}
